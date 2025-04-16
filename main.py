d1cbd92dd2cd9145a1c237d7b296ead0940a42e8
UASLPJuanHernandez <pabloblhdz@icloud.com>
2025-04-15 21:24:27 -0600

    Segundo commit

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from gui import crear_interfaz  # Importa la interfaz
 
 app = crear_interfaz()  # Llama a la función de la GUI
 app.mainloop()  # Ejecuta la aplicación
+
+import numpy as np
+
+x = np.array[2,3]
+print(x)
